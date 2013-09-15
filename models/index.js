68276cd58fb05d8d298c931a0953d40469ff1ea9
irony <cl@iteam.se>
2013-09-15 10:43:13 +0200

    -n readded jobs

--- models/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  group : require('./models/group'),
-  photo : require('./models/photo'),
-  photoCopy : require('./models/photoCopy'),
-  sharespan : require('./models/sharespan'),
-  user : require('./models/user'),
-  passport : require('./auth/passport')
-};
