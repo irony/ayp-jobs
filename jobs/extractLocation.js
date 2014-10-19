728eb66a1b50dd9519e6c36b3844ad7fc764cc6b
irony <cl@iteam.se>
2014-10-19 19:36:12 +0200

    only extract location for new photos

--- jobs/extractLocation.js
+++ jobs/extractLocation.js
@@ -5,47 +5,40 @@ var Photo = require('AllYourPhotosModels').photo,
     Place = require('AllYourPhotosModels').place,
     async = require('async');
 
-module.exports = function(user, done){
+module.exports = function(user, photos, done){
   console.debug('starting location extraction for user', user);
 
   if (!done) throw new Error('Callback is mandatory');
   // find all their photos and sort them on interestingness
-  Photo.find({
-    'owners': user._id, 
-    'exif.gps.GPSLongitude' : {$exists: true}, 
-    'location.place': { $exists:false },
-  }, 'exif.gps location')
-  .sort({taken : - 1})
-  .exec(function(err, photos){
-    if (err) return done(err);
 
-    if (!photos || !photos.length) return done();
+  if (!photos || !photos.length) return done();
 
-    console.debug('found %d photos without normalized gps', photos.length);
+  console.debug('found %d photos without normalized gps', photos.length);
 
-    async.mapSeries(photos || [], function(photo, next){
-      var setter = {$set : {}};
-      var location = setter.$set.location = photo.getLocation();
-      if (!location) return done();
+  async.mapSeries(photos || [], function(photo, next){
+    var setter = {$set : {}};
+    var location = setter.$set.location = photo.getLocation();
+    if (!location) return done();
 
-      console.error('looking up %s %s', location.lng, location.lat);
+    console.error('looking up %s %s', location.lng, location.lat);
 
-      new Place().lookup(location.lng, location.lat, function(err, place){
-        if (err) return next(err);
-        location.place = place;
+    new Place().lookup(location.lng, location.lat, function(err, place){
+      if (err) return next(err);
+      location.place = place;
 
-        // TODO: get timezone, place names etc from geonames API:s
-        // http://api.geonames.org/timezoneJSON?lat=47.01&lng=10.2&username=demo
-        // or:
-        // https://developers.google.com/maps/documentation/timezone/
-        // console.debug('saving location', setter.$set);
-        Photo.update({_id : photo._id}, setter, {upsert: true}, function(err, nr){
-          return next(err, nr);
-        });
+      console.error('place found', place);
+      // TODO: get timezone, place names etc from geonames API:s
+      // http://api.geonames.org/timezoneJSON?lat=47.01&lng=10.2&username=demo
+      // or:
+      // https://developers.google.com/maps/documentation/timezone/
+      // console.debug('saving location', setter.$set);
+      Photo.update({_id : photo._id}, setter, {upsert: true}, function(err, nr){
+        console.error('saved', photo._id);
+        return next(err, nr);
       });
-    }, function(err, nrs){
-      if (err) console.log('location error', err);
-      return done(err, nrs.reduce(function(a,b){return a+b}));
     });
+  }, function(err, nrs){
+    if (err) console.log('location error', err);
+    return done(err, nrs.reduce(function(a,b){return a+b}));
   });
 };
